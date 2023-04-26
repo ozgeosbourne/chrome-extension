6286164e662f82470ebcda6b92d153724028f9d1
ozgeosbourne <ozgenurayyasar@gmail.com>
2023-04-26 14:18:34 +0300

    Added save tab button

--- index.js
+++ index.js
@@ -3,26 +3,24 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
 
-deleteBtn.addEventListener("dbclick", function () {
-    console.log("double clicked!")
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-})
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
+tabBtn.addEventListener("click", function() {
+    //console.log(tabs[0].url)
+    myLeads.push(tabs[0],url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads)
 })
 
 function renderLeads() {
@@ -38,3 +36,19 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems  
 }
+
+deleteBtn.addEventListener("dbclick", function () {
+    //console.log("double clicked!")
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+})
+
+
